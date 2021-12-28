562c3dda2eff4018a270c18c1e20deeef8a01b26
psjw <dlrudtn108@gmail.com>
2021-12-28 15:16:04 +0900

    Q3

--- Q1-5 문제만/sample-1-5/src/main/java/com/example/jpa/controller/FirstController.java
+++ Q1-5 문제만/sample-1-5/src/main/java/com/example/jpa/controller/FirstController.java
@@ -4,6 +4,7 @@ package com.example.jpa.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,5 +20,12 @@ public class FirstController {
     @ResponseBody
     public String helloworld(){
         return "hello world";
+    }
+
+
+    @GetMapping("/hello-spring")
+    @ResponseBody
+    public String hellospring(){
+        return "hello spring";
     }
 }
