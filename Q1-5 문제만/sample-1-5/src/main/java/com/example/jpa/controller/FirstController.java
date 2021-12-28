2fca94258c4fef95f6524051de0689162951125c
psjw <dlrudtn108@gmail.com>
2021-12-28 15:08:08 +0900

    Q2

--- Q1-5 문제만/sample-1-5/src/main/java/com/example/jpa/controller/FirstController.java
+++ Q1-5 문제만/sample-1-5/src/main/java/com/example/jpa/controller/FirstController.java
@@ -6,11 +6,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class FirstController {
     @RequestMapping(method = RequestMethod.GET, path = "/first-url")
     public ResponseEntity firstUrl(){
         return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/helloworld")
+    @ResponseBody
+    public String helloworld(){
+        return "hello world";
     }
 }
