0856bc9a94dc86063a084005bf6dd01838a8a634
psjw <dlrudtn108@gmail.com>
2021-12-28 15:41:05 +0900

    Q4

--- Q1-5 문제만/sample-1-5/src/main/java/com/example/jpa/controller/SecondController.java
+++ Q1-5 문제만/sample-1-5/src/main/java/com/example/jpa/controller/SecondController.java
@@ -1,15 +1,15 @@
 package com.example.jpa.controller;
 
-/**
- * packageName : com.example.jpa.controller
- * fileName : SecontController
- * author : psjw
- * date : 2021-12-28
- * description :
- * ===========================================================
- * DATE              AUTHOR          NOTE
- * -----------------------------------------------------------
- * 2021-12-28        psjw         최초 생성
- */
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class SecondController {
+
+    @GetMapping("/hello-rest")
+    public String hellorest(){
+       return "hello rest";
+    }
+
 }
